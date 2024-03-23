5554b60fae3933865cd8c515d6d4f8be4084f050
Daniel Ji <daniel.ji0923@gmail.com>
2024-03-23 12:45:22 -0700

    initial

---
+++ pets/views.py
@@ -0,0 +1,10 @@
+# from django.shortcuts import render
+from .models import Turtle
+from rest_framework import viewsets, permissions
+from .serializers import TurtleSerializer
+
+# Create your views here.
+class TurtleViewSet(viewsets.ModelViewSet):
+    queryset=Turtle.objects.all().order_by('id')
+    serializer_class=TurtleSerializer
+    permission_classes=[permissions.AllowAny]
